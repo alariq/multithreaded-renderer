5069ee40d590ec14fac0f9c83abaa2f2ffe3b346
sebi <alariq@gmail.com>
2021-02-13 22:55:24 +0200

    SPH: simple emitter support

--- sph_emitter.cpp
+++ sph_emitter.cpp
@@ -6,22 +6,52 @@
 #include <vector>
 #include <cassert>
 
-void SPHEmitterSystem::step(float dt, float cur_time) {
+SPHEmitter *SPHEmitterSystem::createrEmitter()
+{
+    SPHEmitter* e = new SPHEmitter();
+    e->time_since_last_emit_ = 0.0f;
+    e->fluid_model_ = nullptr;
+    sph_get_emitter_system()->add(e);
+    return e;
+}
+       
+void SPHEmitterSystem::destroyEmitter(SPHEmitter* e)
+{
+    sph_get_emitter_system()->remove(e);
+    delete e;
+}
+
+void SPHEmitterSystem::add(SPHEmitter* emitter) {
+    emitters_.push_back(emitter);
+}
+
+void SPHEmitterSystem::remove(SPHEmitter* emitter) {
+    auto b = std::begin(emitters_);
+    auto e = std::end(emitters_);
+    auto it = std::remove(b, e, emitter);
+    assert(it!=emitters_.end());
+    emitters_.erase(it, e);
+}
+
+void SPHEmitterSystem::step(float dt) {
 	for (SPHEmitter* e : emitters_) {
 		float num_to_emit = std::floor((e->time_since_last_emit_ + dt) / e->rate_);
-		e->time_since_last_emit_ -= num_to_emit * e->rate_;
+		e->time_since_last_emit_ += dt - num_to_emit * e->rate_;
 		assert(e->time_since_last_emit_ >= 0.0f);
 
 		int idx = e->fluid_model_->add((int)num_to_emit);
 		for (int i = 0, count = (int)num_to_emit; i < count; ++i) {
-			e->fluid_model_->particles_[idx].density = 0;
-			e->fluid_model_->particles_[idx].pos = e->pos_;
-			e->fluid_model_->particles_[idx].vel = e->initial_vel_;
-			e->fluid_model_->particles_[idx].force = vec2(0,0);
-			e->fluid_model_->particles_[idx].pressure = 0;
-			e->fluid_model_->particles_[idx].flags = kSPHFlagActive;
+			e->fluid_model_->particles_[idx + i].pos = e->pos_;
+			e->fluid_model_->particles_[idx + i].vel = e->initial_vel_;
+
+			e->fluid_model_->particles_[idx + i].density = 0;
+			e->fluid_model_->particles_[idx + i].force = vec2(0,0);
+			e->fluid_model_->particles_[idx + i].pressure = 0;
+
+			e->fluid_model_->particles_[idx + i].flags = kSPHFlagPending;
 		}
 	}
 }
+
 
 
--- sph_emitter.h
+++ sph_emitter.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <vector>
 #include "utils/vec.h"
+#include <vector>
 
 struct SPHEmitter {
 
@@ -10,17 +10,29 @@ struct SPHEmitter {
 	vec2 initial_vel_;
 
 	struct SPHFluidModel* fluid_model_;
-	
+
 	// transient params
 	vec2 pos_;
 	vec2 dir_;
 	float time_since_last_emit_; // seconds
 
+
+  private:
+    friend class SPHEmitterSystem;
+    ~SPHEmitter() = default;
+    SPHEmitter() = default;
 };
 
 class SPHEmitterSystem {
 
-	std::vector<SPHEmitter*> emitters_;
+	std::vector<SPHEmitter *> emitters_;
+public:
 
-	void step(float dt, float cur_time);
+	void step(float dt);
+
+	static SPHEmitter *createrEmitter();
+	static void destroyEmitter(SPHEmitter *);
+
+    void add(SPHEmitter* emitter);
+    void remove(SPHEmitter* emitter);
 };
