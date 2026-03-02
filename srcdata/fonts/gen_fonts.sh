5a47c8f87cf95fcf583efa1ad076e61231078d3f
sebi <alariq@gmail.com>
2026-03-02 19:03:38 +0200

    add some font data (Roboto Medium)

---
+++ srcdata/fonts/gen_fonts.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+./../../build/out/tools/text_tool/text_tool Roboto-Medium.ttf 24 ../../data/fonts/roboto_medium_24
+./../../build/out/tools/text_tool/text_tool Roboto-Medium.ttf 64 ../../data/fonts/roboto_medium_64
+
