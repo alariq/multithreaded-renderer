a079a3d05d55704038f141aaa278dd0310a9839e
sebi <alariq@gmail.com>
2026-04-07 11:17:33 +0300

    xy_quad now has UV 0..1 instead of -1..1

--- engine/utils/gl_utils.h
+++ engine/utils/gl_utils.h
@@ -707,15 +707,16 @@ void generate_cube(MeshBuffer& mb, const vec3 scale, const vec3 offset)
 // size - size of the quad
 // center - local 0 of the quad
 // plane - 0 - XY, 1- XZ 2- YZ
+// TODO: add uv center / size params for custom uvs?
 template<typename MeshBuffer>
 void generate_quad(MeshBuffer& mb, const vec3& size, const vec3& center, int plane)
 {
     mb.allocate_vb(4);
     mb.allocate_ib(6);
 
-	vec2 uv[4] = {{vec2(-1.0f, -1.0f)},
-				  {vec2(-1.0f, 1.0f)},
-				  {vec2(1.0f, -1.0f)},
+	vec2 uv[4] = {{vec2(0.0f, 0.0f)},
+				  {vec2(0.0f, 1.0f)},
+				  {vec2(1.0f, 0.0f)},
 				  {vec2(1.0f, 1.0f)}};
 
     vec3 v0 = vec3(0, 0, 0)*size + center;
