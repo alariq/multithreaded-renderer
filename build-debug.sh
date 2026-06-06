dbe6d4ed663c7e7aa6c7d430059cb6acf75209cb
sebi <alariq@gmail.com>
2026-06-06 23:29:43 +0300

    ASAN: add known 3rdparty leak to suppression list

--- build-debug.sh
+++ build-debug.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-cmake -B ./build/ -S . -DCMAKE_BUILD_TYPE=Debug -DUSE_TRACY=ON -DWITH_ASAN=OFF -DUSE_IMGUI=ON
+cmake -B ./build/ -S . -DCMAKE_BUILD_TYPE=Debug -DUSE_TRACY=ON -DWITH_ASAN=ON -DUSE_IMGUI=ON
 cmake --build ./build --parallel
 
+export LSAN_OPTIONS=suppressions=asan.supp
